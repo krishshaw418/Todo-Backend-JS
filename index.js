bea3b200af672a81138823b439909659c223957f
Krish Shaw <shawkrish418@gmail.com>
2025-04-01 18:16:32 +0530

    frontEnd

--- index.js
+++ index.js
@@ -9,16 +9,22 @@ connectDB();
 const cookieParser = require("cookie-parser");
 const userRoutes = require ('./routes/userRoutes');
 const actionRoutes = require ('./routes/actionRoutes');
+const path = require('path');
 app.use(express.json());
-app.use(cors({ credentials: true, origin : "*"}));
+app.use(cors());
 app.use(cookieParser());
 
-app.get(`/`, async(req, res) => {
-    console.log("Hello form the server!");
-    res.send("Hello from the server!");
-})
+// app.get(`/`, async(req, res) => {
+//     console.log("Hello form the server!");
+//     res.send("Hello from the server!");
+// })
 app.use('/user', userRoutes);
 app.use('/user/action', actionRoutes);
+
+app.use(express.static('public'));
+app.use('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+})
 
 app.listen(port, () =>{
     console.log(`Server is running on port ${port}`);
